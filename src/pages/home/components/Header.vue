16fe0a7b1d7c67ce79126726112b1a0b68e27795
xqx <1025529920@qq.com>
2018-12-05 19:50:44 +0800

    header banner nav over

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="header">
+       <div class="header-left">
+           <div class="iconfont">&#xe624;</div>
+       </div>
+       <div class="header-input">
+           <span class="iconfont">&#xe632;</span>
+           输入城市、游玩、景点主题
+       </div>
+       <div class="header-right">城市
+           <span class="iconfont">&#xe634;</span>
+       </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:"Header"
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+    .header
+        background-color: $bgColor
+        display :flex
+        height: .86rem
+        line-height:.86rem
+        padding-left :.2rem
+        color:#fff
+         .header-left 
+            width :.64rem
+            float:left
+            font-size:.4rem 
+        .header-right
+            width :1.24rem
+            float: right 
+            text-align:center
+        .header-input
+            flex :1
+            line-height:.64rem
+            background-color: #fff
+            border-radius: .1rem
+            margin:.12rem
+            height :.64rem
+            margin-left :.2rem
+            color:#ccc
+            padding-left:.2rem
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList:[{
+          id:'0001',
+          imgUrl:"//img1.qunarzz.com/vc/a4/36/26/07c5904b545d66b3abe72e9dfa.jpg"
+      },{
+          id:'0002',
+          imgUrl:"//img1.qunarzz.com/vc/2f/8e/51/cb4d31b360c836f7d1bbe83c13.jpg"
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
